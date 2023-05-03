83e03acee267412a9ad9d23e7ac2d3387e81a099
Lucija Filipovic <lucija_filipovic_@hotmail.com>
2023-05-03 15:36:55 +0200

    detailed fossils and villagers view

--- app/src/main/java/com/example/animal_crossing/ui/screens/DetailedFossilsScreen.kt
+++ app/src/main/java/com/example/animal_crossing/ui/screens/DetailedFossilsScreen.kt
@@ -1,16 +1,61 @@
 package com.example.animal_crossing.ui.screens
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.example.animal_crossing.data.api.viewModel.BugViewModel
 import com.example.animal_crossing.data.api.viewModel.FishViewModel
 import com.example.animal_crossing.data.api.viewModel.FossilViewModel
+import com.example.animal_crossing.ui.customComposables.ProfileHeader
+import com.example.animal_crossing.ui.customComposables.ProfileProperty
+import com.example.animal_crossing.ui.customComposables.Title
 
 @Composable
 fun DetailedFossilsScreen(vm: FossilViewModel) {
+    val scrollState = rememberScrollState()
     val fossil = vm.selectedFossil
-    Column() {
-        Text(text = fossil.name)
-        Text(text = fossil.url)
+    Column(modifier = Modifier.fillMaxSize()) {
+        BoxWithConstraints {
+            Surface {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .verticalScroll(scrollState)
+                ) {
+                    ProfileHeader(image = fossil.imageUrl, containerHeight = this@BoxWithConstraints.maxHeight)
+                    ProfileContent(vm = vm, containerHeight = this@BoxWithConstraints.maxHeight)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun ProfileContent(
+    vm: FossilViewModel,
+    containerHeight: Dp
+) {
+    val fossils = vm.selectedFossil
+    Column {
+        Title(fossils.name)
+        ProfileProperty(label = "Url", value = fossils.url)
+        ProfileProperty(label = "Fossil Group", value = fossils.fossilGroup)
+        ProfileProperty(label = "Interactable", value = fossils.interactable.toString())
+        ProfileProperty(label = "Sell", value = fossils.sell.toString())
+        ProfileProperty(label = "HHA Base", value = fossils.hhaBase.toString())
+        ProfileProperty(label = "Width", value = fossils.width.toString())
+        ProfileProperty(label = "Length", value = fossils.length.toString())
+
+        fossils.colors.forEach { color ->
+            ProfileProperty(label = "Color", value = color)
+        }
+
+        Spacer(modifier = Modifier.height((containerHeight - 320.dp).coerceAtLeast(0.dp)))
     }
 }
--- app/src/main/java/com/example/animal_crossing/ui/screens/DetailedVillagersScreen.kt
+++ app/src/main/java/com/example/animal_crossing/ui/screens/DetailedVillagersScreen.kt
@@ -1,15 +1,60 @@
 package com.example.animal_crossing.ui.screens
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.example.animal_crossing.data.api.viewModel.FossilViewModel
 import com.example.animal_crossing.data.api.viewModel.VillagerViewModel
+import com.example.animal_crossing.ui.customComposables.ProfileHeader
+import com.example.animal_crossing.ui.customComposables.ProfileProperty
+import com.example.animal_crossing.ui.customComposables.Title
 
 @Composable
 fun DetailedVillagersScreen(vm: VillagerViewModel) {
+    val scrollState = rememberScrollState()
     val villager = vm.selectedVillager
-    Column() {
-        Text(text = villager.name)
-        Text(text = villager.clothing)
+    Column(modifier = Modifier.fillMaxSize()) {
+        BoxWithConstraints {
+            Surface {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .verticalScroll(scrollState)
+                ) {
+                    ProfileHeader(image = villager.imageUrl, containerHeight = this@BoxWithConstraints.maxHeight)
+                    ProfileContent(vm = vm, containerHeight = this@BoxWithConstraints.maxHeight)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun ProfileContent(
+    vm: VillagerViewModel,
+    containerHeight: Dp
+) {
+    val villagers = vm.selectedVillager
+    Column {
+        Title(villagers.name)
+        ProfileProperty(label = "url", value = villagers.url)
+        ProfileProperty(label = "Species", value = villagers.species)
+        ProfileProperty(label = "Personality", value = villagers.personality)
+        ProfileProperty(label = "Gender", value = villagers.gender)
+        ProfileProperty(label = "Birthday Month And Day", value = villagers.birthdayMonth + " "+ villagers.birthdayDay)
+        ProfileProperty(label = "Sign", value = villagers.sign)
+        ProfileProperty(label = "Quote", value = villagers.quote)
+        ProfileProperty(label = "Phrase", value = villagers.phrase)
+        ProfileProperty(label = "Islander", value = villagers.islander.toString())
+        ProfileProperty(label = "Debut", value = villagers.debut)
+
+
+        Spacer(modifier = Modifier.height((containerHeight - 320.dp).coerceAtLeast(0.dp)))
     }
 }
